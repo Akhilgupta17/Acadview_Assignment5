c25168016adebca463b12e5fa392a54293cc925a
Akhilgupta17 <Akhilgupta1711@gmail.com>
2018-06-12 11:54:26 +0530

    My Fifth Assignment

--- Question5.py
+++ Question5.py
@@ -1,9 +1,10 @@
 Quantity=int(input("Enter Quantity :"))
-if Quantity*100>1000:
-    cost=Quantity*100
+cost=Quantity*100
+if cost>1000:
     print("total cost : %d" % cost)
     print("You Get 10% Discount")
     finalcost=(cost-(cost*10/100))
     print("After discount You Pay: %d" % finalcost)
 else:
     print("You Will Not Get Any Discount")
+    print("You Have to PAY : %d" %cost)
