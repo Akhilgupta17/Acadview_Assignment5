41d47b7cab138e5ea81be44f2a6d70645695df33
Akhilgupta17 <Akhilgupta1711@gmail.com>
2018-06-12 11:40:19 +0530

    My Fifth Assignment

--- Question1.py
+++ Question1.py
@@ -6,9 +6,3 @@ elif Day>366 or Day<365:
 else:
     print("Normal Year")
     
-l=int(input("Enter Length:")
-b=int(input("Enter Breadth:")
-if l==b:
-    print("Square")
-else
-    print("Rectangle")            
