42600beb03bd7608b494bfd0799866c3bf220130
Akhilgupta17 <Akhilgupta1711@gmail.com>
2018-06-12 12:53:48 +0530

    My Fifth Assignment

--- Question1.py
+++ Question1.py
@@ -6,3 +6,17 @@ elif Day>366 or Day<365:
 else:
     print("Normal Year")
     
+#OR
+
+year=int(input("Enter Year:"))
+if (year % 4) == 0:
+    if (year % 100) == 0:
+        if (year % 400) == 0:
+            print("{0} is A Leap year".format(year))
+        else:
+            print("{0} is Not a Leap Year".format(year))
+    else:
+        print("{0} is  a Leap Year".format(year))
+else:
+    print("{0} is Not a Leap Year".format(year))
+            
